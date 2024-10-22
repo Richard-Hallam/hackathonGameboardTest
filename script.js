a9a1f570ebc33bb09b45272d46edf8d0c4d3f8ec
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-10-22 10:06:37 +0100

    added clearBoard(), setCorrectSquare(), setIncorrectSquare()

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 
 document.addEventListener('DOMContentLoaded', function(){
-    let postion = 0
+    let positon = 0
     movePlayer(0, getSquares());
     document.addEventListener('keydown',function(event){ 
         if(event.key === 'ArrowRight'){
             let squares = getSquares();
             event.preventDefault();
-            postion = (postion + 1)%squares.length;
-            movePlayer(postion, squares);
+            positon = (positon + 1)%squares.length;
+            movePlayer(positon, squares);
             
         }
         if(event.key === 'ArrowLeft'){
             let squares = getSquares();
             event.preventDefault();
-            if(postion === 0){
-                postion = squares.length -1;
+            if(positon === 0){
+                positon = squares.length -1;
             } else {
-                postion--
+                positon--
             }
-            movePlayer(postion, squares);
+            movePlayer(positon, squares);
         }
     });
 });
 
+/**
+ * gets squares from the DOM
+ * @returns array
+ */
 function getSquares(){
     sq = document.getElementsByClassName('square');
     return sq;
 }
 
+/**
+ * takes an index and array then 'moves' the player by adding a class to change the 
+ * colour of the square at the players location.
+ */
 function movePlayer(index, squares){
     for(let i=0; i<squares.length; i++){
         squares[i].classList.remove('player-square');
@@ -35,3 +43,33 @@ function movePlayer(index, squares){
     squares[index].classList.add('player-square');
 }
 
+
+/**
+ * clears the board 
+ */
+function clearBoard(squares){
+    for(let i=0; i<squares.lenght; i++){
+        squares[i].classList.remove('incorrect-square');
+        squares[i].classList.remove('correct-square');
+        squares[i].classList.remove('player-square');
+    }
+}
+
+/**
+ * Adds correct-square class to square
+ * @param {int} index 
+ * @param {Array} squares 
+ */
+function setCorrectSquare(index, squares){
+    squares[index].classList.add('correct-square');
+}
+
+
+/**
+ * Adds incorrect-square class to square
+ * @param {int} index 
+ * @param {Array} squares 
+ */
+function setIncorrectSquare(index, squares){
+    squares[index].classList.add('incorrect-square');
+}
